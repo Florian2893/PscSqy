47bc6579d3e57334ad84a44bc6a8b175b8e85959
Florian2893 <benoist.florian.28@gmail.com>
2015-04-21 09:16:47 +0200

    modif accueil et superheader

--- parts/accueil.php
+++ parts/accueil.php
@@ -1,1 +1,3 @@
-TEST
+<div class="conteneur-page">
+	TEST
+</div>	
--- parts/super_header.php
+++ parts/super_header.php
@@ -1,3 +1,4 @@
 <div id="super-header">
+	<!-- Notification SGR -->
 	<div class="notif" alt="notification SGR">&#xf39f; <span class="nombre-notif">2</span></div>
 </div>
