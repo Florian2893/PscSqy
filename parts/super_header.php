4e21271d391366eb0b3c5752c218c7d963c829de
Florian2893 <benoist.florian.28@gmail.com>
2015-04-23 13:24:23 +0200

    css notif modifier

--- parts/super_header.php
+++ parts/super_header.php
@@ -1,23 +1,14 @@
 <div id="super-header">
 	<!-- Notification SGR -->
-	<div class="notif" alt="notification SGR">&#xf39f;<span class="nombre-notif">2</span></div>
+	<span class="notif" alt="notification SGR">&#xf39f;<span class="nombre-notif">2</span></span>
+	<span class="notif" alt="notification SGR">&#xf39f;<span class="nombre-notif">2</span></span>
 </div>
 
 <script type="text/javascript">
-	/*function alert_notif(){ 
-		$('.nombre-notif').css('background-color','#ff0000').delay(20).css('background-color','#000000'); 
-	}
-
-	$(function(){
-		if($('.nombre-notif').css('display','block')){
-			setInterval('alert_notif()',3000); 
-		}
-	})*/
-
-function FaireClignoterImage (){ 
-   $(".nombre-notif").fadeOut(900).delay(300).fadeIn(800); 
-} 
-$(document).ready(function(){ 
-    setInterval('FaireClignoterImage()',2200); 
-});
+	function FaireClignoterImage (){ 
+		$(".nombre-notif").fadeOut(900).delay(300).fadeIn(800); 
+	} 
+	$(document).ready(function(){ 
+		setInterval('FaireClignoterImage()',2200); 
+	});
 </script>
