550ab6d17f0abc3d85dc6d8e79069cf6c17b82e0
Florian2893 <benoist.florian.28@gmail.com>
2015-04-24 11:22:37 +0200

    retrait fonction clignotement

--- parts/super_header.php
+++ parts/super_header.php
@@ -3,12 +3,3 @@
 	<span class="notif" alt="notification SGR">&#xf39f;<span class="nombre-notif">2</span></span>
 	<span class="notif" alt="notification SGR">&#xf39f;<span class="nombre-notif">2</span></span>
 </div>
-
-<script type="text/javascript">
-	function FaireClignoterImage (){ 
-		$(".nombre-notif").fadeOut(900).delay(300).fadeIn(800); 
-	} 
-	$(document).ready(function(){ 
-		setInterval('FaireClignoterImage()',2200); 
-	});
-</script>
